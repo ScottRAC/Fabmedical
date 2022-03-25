20ae4aea5f6bb7fbc67c96e41ca03d278faec438
Scott Lyle <scott.lyle@rac.com.au>
2022-03-25 03:52:02 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -5,6 +5,10 @@ const request = require('request');
 
 const app = express();
 
+const appInsights = require("applicationinsights");
+appInsights.setup("de7ea298-7564-493c-9279-e90ef64cca22");
+appInsights.start();
+
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
 
